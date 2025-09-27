ef2c251b12cdce5f09ac9558ecc34a33b916acf2
Austin Liao <131895709+Austin-Almighty@users.noreply.github.com>
2025-09-27 22:35:11 +0800

    Implement player movement and rotation functionality

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from player import Player
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -18,10 +22,13 @@ def main():
                 return
             
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-
+        
         dt = clock.tick(60) / 1000
+        
     # print("Starting Asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
